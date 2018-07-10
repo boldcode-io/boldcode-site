61bb0c5bfb515881a0b2a149b31fc02e987c6180
Glenn Y. Rolland <glenux@glenux.net>
2018-07-10 15:14:02 +0200

    mardi 10 juillet 2018, 15:14:02 (UTC+0200)

--- bin/publi.sh
+++ bin/publi.sh
@@ -4,14 +4,13 @@ set -x
 
 rm -rf _site/
 jekyll build
-git push origin master
 git worktree add -B gh-pages _site origin/gh-pages
 
-cd gh-pages 
+cd _site
 git add --all
 git commit -m "`date`"
 git push origin gh-pages
 cd ..
 
-git worktree remove gh-pages
+git worktree remove _site
 
