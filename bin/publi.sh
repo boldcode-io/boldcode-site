972b8f0febb27061d55e4c2769a866f58676e80f
Glenn Y. Rolland <glenux@glenux.net>
2018-07-10 15:12:38 +0200

    mardi 10 juillet 2018, 15:12:38 (UTC+0200)

--- bin/publi.sh
+++ bin/publi.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 # change the branch names appropriately
-git checkout master 
 rm -rf _site/
 jekyll build
+git push origin master
+git worktree add -B gh-pages _site origin/gh-pages
+
+cd gh-pages 
 git add --all
 git commit -m "`date`"
-git push origin master
-git subtree push --prefix  _site/ origin gh-pages
+git push origin gh-pages
+cd ..
+
+git worktree remove gh-pages
+
