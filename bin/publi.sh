f5c956b8de6751006b35a9dc9060adeeeb072b6f
Glenn Y. Rolland <glenux@glenux.net>
2018-07-10 14:28:24 +0200

    Fix publish script

--- bin/publi.sh
+++ bin/publi.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # change the branch names appropriately
-git checkout site
+git checkout master 
 rm -rf _site/
 jekyll build
 git add --all
 git commit -m "`date`"
-git push origin site
+git push origin master
 git subtree push --prefix  _site/ origin gh-pages
